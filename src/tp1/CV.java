326d526246e0785f4cbc2f3b62fb21d1dbfabbfa
user.name <jeremiebergeronlol@outlook.com>
2020-02-03 11:30:38 -0500

    Modification des instances

--- src/tp1/CV.java
+++ src/tp1/CV.java
@@ -4,8 +4,7 @@ public class CV {
 
 	// Champs d'instance privés
 	
-	 private String nom;
-	 private String prenom;
+	
 	 private String formation;
 	 private int expTravail;
 	 private String[] competence;
