efe8ba6d0c7e6b50f773023a4f1f7104af898323
user.name <jeremiebergeronlol@outlook.com>
2020-02-13 12:32:09 -0500

    J'ai supprimé du code dans mon commit précédent. C'est un réajout de celui-ci

--- src/tp1/Principal.java
+++ src/tp1/Principal.java
@@ -1,14 +1,24 @@
-package tp1; 
+package tp1;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		lireCommande test = new lireCommande();
-		
-		
-		
+
+		System.out.println("Bienvenue chez Barette!");
+
+		String tableauSimonCompetence[] = { "Autonome", "Charmant", "Débrouillard" };
+		String tableauJeremieCompetence[] = { "Autonome", "Responsable", "Débrouillard" };
+
+		CV simon = new CV("Delteil", "Simon", "Technique en informatique de gestion", 0, tableauSimonCompetence,
+				"Avoir du travail");
+		CV jeremie = new CV("Bergeron", "Jérémie", "Technique en informatique de gestion", 2, tableauJeremieCompetence,
+				"Avoir du travail");
+
+		simon.afficher();
+		System.out.println();
+		jeremie.afficher();
 	}
 
-	
 }
