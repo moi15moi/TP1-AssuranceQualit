de55f841742f794ece7fe3b35d3e9d5b93ad7e29
Simon Delteil <tothemoon99999@gmail.com>
2020-04-01 13:49:07 -0400

    optimisation du calcul des taxes

--- src/tp1/Facture.java
+++ src/tp1/Facture.java
@@ -1,5 +1,8 @@
 package tp1;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
@@ -8,10 +11,12 @@ public class Facture {
 	private ArrayList<Client> client = new ArrayList<Client>();
 	private ArrayList<Plats> plats = new ArrayList<Plats>();
 	private ArrayList<Commande> commande = new ArrayList<Commande>();
-	
+
+	private String facture = "", erreurFacture = "";
+
 	public Facture(ArrayList<String> fichier) {
 		try {
-			
+
 			int client = fichier.indexOf("Clients :");
 			int plat = fichier.indexOf("Plats :");
 			int commande = fichier.indexOf("Commandes :");
@@ -22,10 +27,9 @@ public class Facture {
 				this.client.add(cliTemp);
 			}
 
-			
 			for (int i = plat + 1; i < commande; i++) {
 				String[] ligneFichier = fichier.get(i).split(" ");
-				
+
 				if (ligneFichier.length == 2) {
 					Plats platTemp = new Plats(ligneFichier[0], ligneFichier[1]);
 
@@ -35,93 +39,119 @@ public class Facture {
 				}
 			}
 
-			
 			for (int i = commande + 1; i < fin; i++) {
 
 				String[] ligneFichier = fichier.get(i).split(" ");
-				
-				if (ligneFichier.length == 3) {
-				Commande comTemp = new Commande(ligneFichier[0], ligneFichier[1], ligneFichier[2]);
 
-				this.commande.add(comTemp);
+				if (ligneFichier.length == 3) {
+					Commande comTemp = new Commande(ligneFichier[0], ligneFichier[1], ligneFichier[2]);
+
+					this.commande.add(comTemp);
 				} else {
 					System.out.println("Le fichier ne respecte pas le format demandé ! (ligne: " + (i + 1) + ")");
 				}
 			}
-			
-			
+
 		} catch (Exception e) {
 			System.out.println("Le fichier ne respecte pas le format demandé. !");
 		}
-		
+
 	}
 
-	public void afficherFacture() {
+	/*
+	 * Argument: Aucun.
+	 * 
+	 * Retour: Retourne les variables globales "facture" et "erreurFacture".
+	 * 
+	 * Description: Fait appele � la m�thode calculerFacture et retourne la facture.
+	 */
+	public String getFacture() {
+		return facture + "\n" + erreurFacture;
+	}
+
+	/*
+	 * Argument: Aucun.
+	 * 
+	 * Retour: Rien.
+	 * 
+	 * Description: Modifie les variables globales "facture" et "erreurFacture".
+	 */
+	public void calculerFacture() {
 		double prixFactureClient;
 		DecimalFormat formatter = new DecimalFormat("#0.00");
 		
-		System.out.println("Factures :\n");
+		facture = "";
+        erreurFacture = "";
 
-		for (int i = 0; i < client.size(); i++) {
+		facture += "Factures :\n";
 
-			if ((prixFactureClient = calculTaxes(calculerPrixTotalCommande(getCommandeSelonClient(client.get(i))))) != 0) {
-
-				System.out.println(client.get(i).getNom() + " " + formatter.format(prixFactureClient) + "$");
-			}
-		}
-
+		// Associe les commandes aux clients et v�rifie si le client dans la commande
+		// existe
 		for (int i = 0; i < commande.size(); i++) {
 
 			boolean trouver = false;
-
-			for (int j = 0; j < client.size(); j++) {
+			for (int j = 0; j < client.size() && !trouver; j++) {
 
 				if (commande.get(i).getNomClient().equalsIgnoreCase(client.get(j).getNom())) {
 					trouver = true;
+					client.get(j).ajouterCommande(commande.get(i));
 				}
 			}
+
 			if (!trouver) {
-				System.out.println("Erreur, le client " + commande.get(i).getNomClient() + " n'existent pas.");
-			}
-
-		}
-	}
-
-	public ArrayList<Commande> getCommandeSelonClient(Client pClient) {
-		ArrayList<Commande> commandeClient = new ArrayList<Commande>();
-		
-		for (int i = 0; i < commande.size(); i++) {
-
-			if (pClient.getNom().equalsIgnoreCase(commande.get(i).getNomClient())) {
-				commandeClient.add(commande.get(i));
+				erreurFacture += "Erreur, le client " + commande.get(i).getNomClient() + " n'existent pas.\n";
 			}
 		}
 
-		return commandeClient;
+		// Calcule le prix total de la facture et v�rifie si le prix total est �gale � 0
+		for (int i = 0; i < client.size(); i++) {
+
+			if ((prixFactureClient = calculTaxes(calculerPrixBrutCommande(client.get(i)))) != 0) {
+
+				facture += client.get(i).getNom() + " " + formatter.format(prixFactureClient) + "$\n";
+			}
+		}
 	}
 
-	public double calculerPrixTotalCommande(ArrayList<Commande> commandeSelonXClient) {
+	/*
+	 * Argument: Client.
+	 * 
+	 * Retour: Le prix brut.
+	 * 
+	 * Description: Le prix brut de toutes les commandes du Client.
+	 */
+	public double calculerPrixBrutCommande(Client pClient) {
 		double prix = 0;
+		ArrayList<Commande> listeCommande = pClient.getListeCommande();
 
-		for (int i = 0; i < commandeSelonXClient.size(); i++) {
+		for (int i = 0; i < listeCommande.size(); i++) {
 			boolean trouve = false;
 
-			for (int j2 = 0; j2 < plats.size(); j2++) {
+			for (int j2 = 0; j2 < plats.size() && !trouve; j2++) {
 
-				if (commandeSelonXClient.get(i).getRepas().equalsIgnoreCase(plats.get(j2).getNom())) {
-
-					prix += plats.get(j2).getPrix() * commandeSelonXClient.get(i).getQuantite();
+				if (listeCommande.get(i).getRepas().equalsIgnoreCase(plats.get(j2).getNom())) {
 					trouve = true;
+
+					if (listeCommande.get(i).getQuantite() > 0) {
+						prix += plats.get(j2).getPrix() * listeCommande.get(i).getQuantite();
+					} else {
+						erreurFacture += "Erreur, la commande " + i + " de " + listeCommande.get(i).getNomClient()
+								+ " n'est pas valide, car la quanti� command�e est "
+								+ listeCommande.get(i).getQuantite() + ".\n";
+					}
 				}
 			}
 
 			if (!trouve) {
-				System.out.println("Il n'existe pas de plat nommé: " + commandeSelonXClient.get(i).getRepas());
+
+				erreurFacture += "Erreur, il n'existe pas de plat nomm�: " + listeCommande.get(i).getRepas() + ".\n";
+
 			}
 		}
 
 		return prix;
 	}
+
 	
 	/*Argument: le prix brût.
 	 * 
@@ -161,6 +191,27 @@ public class Facture {
 		double prixTotal = prixBrut + montantTPS + montantTVQ;
 		
 		return prixTotal;
+	}
+	
+	/*
+	 * Argument: Aucun.
+	 * 
+	 * Retour: Rien.
+	 * 
+	 * Description: Enregistre la facture en fichier texte.
+	 */
+	public void enregistrerFactureFicher() {
+		File file = new File("facture\\Facture-du-" + DateHeure.modifierAffichageDateHeure() + ".txt");
+
+		FileWriter writer;
+		try {
+			writer = new FileWriter(file);
+			writer.write(getFacture());
+			writer.close();
+
+		} catch (IOException e) {
+			System.out.println("Erreur lors de l'enregistrement de la facture en fichier texte.");
+		}
 	}
 
 }
